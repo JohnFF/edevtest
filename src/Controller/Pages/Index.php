37fba45f8b53a00897ac71db43db272b1c29aed1
John Kirk <john@civifirst.com>
2021-02-20 23:21:22 +0000

    Added client side validation using the server side settings.

--- src/Controller/Pages/Index.php
+++ src/Controller/Pages/Index.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Pages;
 
+use App\Electroneum\Validator;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,6 +11,18 @@ class Index extends AbstractController
 {
     public function show(): Response
     {
-        return $this->render('pages/index.html.twig');
+        return $this->render('pages/index.html.twig', [
+            'validation' => [
+                'username' => [
+                    'max_length' => Validator::MAX_USERNAME_LENGTH,
+                    'regex' => Validator::VALID_USERNAME_REGEX,
+                ],
+                'password' => [
+                    'min_length' => Validator::MIN_PASSWORD_LENGTH,
+                    'max_length' => Validator::MAX_PASSWORD_LENGTH,
+                    'regex' => Validator::VALID_PASSWORD_REGEX,
+                ]
+            ],
+        ]);
     }
 }
