6d10754b27924493e5244a56f90689a77b713b95
John Kirk <john@civifirst.com>
2021-02-21 01:52:56 +0000

    Refactor for code reuse.

--- src/Electroneum/UserLoader.php
+++ src/Electroneum/UserLoader.php
@@ -48,17 +48,7 @@ class UserLoader {
      */
     public static function load_user_with_password($username, $password): User {
 
-        // Verify the username each time before it's loaded to prevent file
-        // traversal attempts.
-        Validator::verify_username_valid($username);
-
-        // Do not check the password is valid upon load unless we subsequently
-        // change password standards.
-
-        // Reference the file formats in UserFactory so as not to have to update
-        // them in two places.
-        $rawStoredUserDetails = file_get_contents(UserFactory::STORAGE_FILEPATH . $username . UserFactory::STORAGE_FILETYPE);
-        $storedUserDetails = json_decode($rawStoredUserDetails, true);
+        $storedUserDetails = self::load_user($username);
 
         if (!password_verify($password, $storedUserDetails['password_hash'])) {
             throw new Exception('Incorrect password.');
